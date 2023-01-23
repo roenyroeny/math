b3af10085ea340f4e1de788d47320039a497e47e
Jeroen <jeroen.vanschijndel@otoy.com>
2023-01-23 11:23:13 +0100

    generator for scalar/vector

---
+++ generator/Program.cs
@@ -0,0 +1,18 @@
+using generator;
+
+class Program
+{
+
+
+    static void Main()
+    {
+
+        var g = new Generator();
+        var scalar_h = g.GenerateScalar_H();
+        System.IO.File.WriteAllText("scalar.h", scalar_h);
+
+        var vector_h = g.GenerateVector_H();
+        System.IO.File.WriteAllText("vector.h", vector_h);
+
+    }
+}
---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+
+#include "scalar.h"
+#include "vector.h"
+
+void main()
+{
+	f32_2 a;
+	f32_2 b;
+
+	auto c = a + b;
+}
